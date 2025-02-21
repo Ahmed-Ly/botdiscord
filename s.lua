ea73d44462a31228244f6045ed409bb4e0d7a0be
Ahmed Ly <lyahmed1998@gmail.com>
2025-02-21 17:48:20 +0200

    Update s.lua

--- s.lua
+++ s.lua
@@ -378,7 +378,6 @@ end
 function getPlayerInServer()
     local players = getElementsByType("player")
     local playerCount = #players
-    outputDebugString("DEBUG: عدد اللاعبين في السيرفر: " .. playerCount)
     return playerCount
 end
 
