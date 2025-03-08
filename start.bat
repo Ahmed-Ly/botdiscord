bac1dbd0c2d43c8acdaa725221c1eb7eef2f4a0f
Ahmed Ly <lyahmed1998@gmail.com>
2025-03-08 05:46:29 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -25,11 +25,44 @@ call activate.bat
 echo Installing required Python packages...
 pip install discord.py Flask requests configparser
 
+
+@echo off
+echo Please enter the following configuration details:
+echo.
+
+set /p username="Username: "
+set /p password="Password: "
+set /p host="Host: "
+set /p port="Port: "
+set /p resource="Resource: "
+set /p bottoken="Bot Token: "
+set /p channel="Channel: "
+
+echo.
+echo Writing configuration to config.ini...
+
+(
+    echo [MTA]
+    echo username=%username%
+    echo password=%password%
+    echo host=%host%
+    echo port=%port%
+    echo resource=%resource%
+    echo bottoken=%bottoken%
+    echo channel=%channel%
+) > config.ini
+
+echo.
+
+echo The configuration file has been updated successfully.
+
 echo Running Python script...
 if exist scr.py (
     python scr.py
 ) else (
     echo scr.py not found!
 )
+
+
 
 pause
